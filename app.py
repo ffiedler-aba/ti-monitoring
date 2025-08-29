14d5f2abd97385d85673ca079f906a7458ac69cf
Markus <markus@example.com>
2025-08-29 19:53:04 +0200

    Make footer elements configurable via YAML file with default values

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 from dash import Dash, html, dcc
 from mylibrary import *
 from myconfig import *
+import yaml
+import os
 
 app = Dash(__name__, use_pages=True, title='TI-Monitoring')
 server = app.server
@@ -11,7 +13,84 @@ app.config.external_stylesheets = [
     'https://fonts.googleapis.com/icon?family=Material+Icons'
 ]
 
+def load_footer_config():
+    """Load footer configuration from YAML file"""
+    config_path = os.path.join(os.path.dirname(__file__), 'config.yaml')
+    try:
+        with open(config_path, 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        return config.get('footer', {})
+    except FileNotFoundError:
+        # Return default configuration if file doesn't exist
+        return {
+            'home': {'label': 'Home', 'link': 'https://lukas-schmidt-russnak.de', 'enabled': True, 'new_tab': True},
+            'documentation': {'label': 'Dokumentation', 'link': 'https://github.com/lsr-dev/ti-monitoring', 'enabled': True, 'new_tab': True},
+            'privacy': {'label': 'Datenschutz', 'link': 'https://lukas-schmidt-russnak.de/datenschutz/', 'enabled': True, 'new_tab': True},
+            'imprint': {'label': 'Impressum', 'link': 'https://lukas-schmidt-russnak.de/impressum/', 'enabled': True, 'new_tab': True},
+            'copyright': {'text': '© Lukas Schmidt-Russnak', 'enabled': True}
+        }
+    except Exception:
+        # Return default configuration if there's any other error
+        return {
+            'home': {'label': 'Home', 'link': 'https://lukas-schmidt-russnak.de', 'enabled': True, 'new_tab': True},
+            'documentation': {'label': 'Dokumentation', 'link': 'https://github.com/lsr-dev/ti-monitoring', 'enabled': True, 'new_tab': True},
+            'privacy': {'label': 'Datenschutz', 'link': 'https://lukas-schmidt-russnak.de/datenschutz/', 'enabled': True, 'new_tab': True},
+            'imprint': {'label': 'Impressum', 'link': 'https://lukas-schmidt-russnak.de/impressum/', 'enabled': True, 'new_tab': True},
+            'copyright': {'text': '© Lukas Schmidt-Russnak', 'enabled': True}
+        }
+
+def create_footer_element(config_item):
+    """Create a footer element based on configuration"""
+    if not config_item.get('enabled', True):
+        return None
+    
+    if 'text' in config_item:  # Copyright element
+        return html.Div(config_item['text'])
+    
+    # Link element
+    link_attrs = {'href': config_item['link']}
+    if config_item.get('new_tab', False):
+        link_attrs['target'] = '_blank'
+    
+    return html.Div([html.A(config_item['label'], **link_attrs)])
+
 def serve_layout():
+    # Load footer configuration
+    footer_config = load_footer_config()
+    
+    # Build footer elements
+    footer_elements = []
+    
+    # Add home link if enabled
+    if 'home' in footer_config:
+        element = create_footer_element(footer_config['home'])
+        if element:
+            footer_elements.append(element)
+    
+    # Add documentation link if enabled
+    if 'documentation' in footer_config:
+        element = create_footer_element(footer_config['documentation'])
+        if element:
+            footer_elements.append(element)
+    
+    # Add privacy link if enabled
+    if 'privacy' in footer_config:
+        element = create_footer_element(footer_config['privacy'])
+        if element:
+            footer_elements.append(element)
+    
+    # Add imprint link if enabled
+    if 'imprint' in footer_config:
+        element = create_footer_element(footer_config['imprint'])
+        if element:
+            footer_elements.append(element)
+    
+    # Add copyright if enabled
+    if 'copyright' in footer_config:
+        element = create_footer_element(footer_config['copyright'])
+        if element:
+            footer_elements.append(element)
+    
     layout = html.Div([
         html.Header(children = [
             html.Div(id='logo-wrapper', children = [
@@ -37,18 +116,12 @@ def serve_layout():
             ]),
 			html.Div(className = 'box', children = [
                 html.H3('Disclaimer'),
-                html.Span('Die Bereitstellung der abgebildeten Informationen erfolgt ohne Gewähr. Als Grundlage dienen Daten der gematik GmbH, die sich über eine öffentlich erreichbare Schnittstelle abrufen lassen. Weitere Informationen dazu hier: '),
+                html.Span('Die Bereitstellung der abgebildeten Informationen erfolgt ohne Gewähr. Als Grundlage dieren Daten der gematik GmbH, die sich über eine öffentlich erreichbare Schnittstelle abrufen lassen. Weitere Informationen dazu hier: '),
                 html.A('https://github.com/gematik/api-tilage', href='https://github.com/gematik/api-tilage', target='_blank'),
                 html.Span('.')
             ]),
 		]),
-		html.Div(id = 'footer', children = [
-            html.Div([html.A('Home', href='https://lukas-schmidt-russnak.de', target='_blank')]),
-            html.Div([html.A('Dokumentation', href='https://github.com/lsr-dev/ti-monitoring', target='_blank')]),
-            html.Div([html.A('Datenschutz', href='https://lukas-schmidt-russnak.de/datenschutz/', target='_blank')]),
-            html.Div([html.A('Impressum', href='https://lukas-schmidt-russnak.de/impressum/', target='_blank')]),
-            html.Div('© Lukas Schmidt-Russnak')
-        ])
+		html.Div(id = 'footer', children = footer_elements)
     ])
     return layout
 
