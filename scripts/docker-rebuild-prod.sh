f3118b9ba41f170a09f99ab933f9a18f0a1b472b
elpatron68 <m.busche@gmail.com>
2025-09-02 20:41:47 +0000

    Update script

--- scripts/docker-rebuild-prod.sh
+++ scripts/docker-rebuild-prod.sh
@@ -1,1 +1,1 @@
-docker compose -f docker-compose.yml down && docker compose -f docker-compose.yml up --build -d
+git pull && docker compose -f docker-compose.yml down && docker compose -f docker-compose.yml up --build -d
