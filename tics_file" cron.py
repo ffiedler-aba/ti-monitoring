539f75d6376100ff095f18776e241a43eed47bbb
elpatron68 <m.busche@gmail.com>
2025-09-02 21:13:52 +0000

    Fehler

---
+++ tics_file" cron.py
@@ -0,0 +1,5 @@
+[33mec48a5b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Verbessere Textkontrast für mobilen Dark Mode
+[33m0ab0f09[m Verbessere mobile Darstellung und Dark Mode für Stats-Tabelle
+[33mb33153b[m Update script
+[33m8ad2bae[m Optimiere Stats-Tabelle und verschiebe Datenbankgröße-Berechnung
+[33m88e1fff[m Update Script-Dateien
