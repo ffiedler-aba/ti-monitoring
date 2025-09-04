4af8f9a4983f79ef871be6cdaf31d37befb8fc23
elpatron68 <m.busche@gmail.com>
2025-09-04 08:42:20 +0000

    back-button: vereinfacht – Zurück führt immer zur Startseite '/' (ohne home_url-Abhängigkeit)

--- pages/plot.py
+++ pages/plot.py
@@ -191,8 +191,7 @@ def serve_layout(ci=None, hours=None, **other_unknown_query_strings):
         return html.Div([
             html.H2('Fehler: Keine Komponente angegeben'),
             html.P('Bitte geben Sie eine Komponenten-ID in der URL an, z.B. /plot?ci=12345'),
-            dcc.Store(id='home-url-store-error', data=config_home_url),
-            html.Button('Zurück', id='back-button-error', className='button')
+            html.A(href='/', children=[html.Button('Zurück', className='button')])
         ])
     
     # Get CI info for display
@@ -201,8 +200,7 @@ def serve_layout(ci=None, hours=None, **other_unknown_query_strings):
     layout = [
         html.H2('Verfügbarkeit der Komponente ' + str(ci)),
         html.H3(ci_info['product'].iloc[0] + ', ' + ci_info['name'].iloc[0] + ', ' + ci_info['organization'].iloc[0]),
-        dcc.Store(id='home-url-store', data=config_home_url),
-        html.Button('Zurück', id='back-button', className='button'),
+        html.A(href='/', children=[html.Button('Zurück', className='button')]),
         
         # Plot section (moved up to avoid overlay issues)
         html.Div(id='plot-container', children=[
@@ -255,53 +253,6 @@ def serve_layout(ci=None, hours=None, **other_unknown_query_strings):
     return layout
 
 layout = serve_layout
-
-from dash import clientside_callback
-
-# Clientside back navigation: use history.back if possible, otherwise go to home_url
-clientside_callback(
-    """
-    function(n, homeUrl) {
-        if (!n) { return window.dash_clientside.no_update; }
-        try {
-            if (window.history && window.history.length > 1) {
-                window.history.back();
-            } else if (homeUrl) {
-                window.location.href = homeUrl;
-            }
-        } catch (e) {
-            if (homeUrl) { window.location.href = homeUrl; }
-        }
-        return window.dash_clientside.no_update;
-    }
-    """,
-    Output('url', 'href'),
-    [Input('back-button', 'n_clicks')],
-    [dash.State('home-url-store', 'data')],
-    prevent_initial_call=True
-)
-
-clientside_callback(
-    """
-    function(n, homeUrl) {
-        if (!n) { return window.dash_clientside.no_update; }
-        try {
-            if (window.history && window.history.length > 1) {
-                window.history.back();
-            } else if (homeUrl) {
-                window.location.href = homeUrl;
-            }
-        } catch (e) {
-            if (homeUrl) { window.location.href = homeUrl; }
-        }
-        return window.dash_clientside.no_update;
-    }
-    """,
-    Output('url', 'pathname'),
-    [Input('back-button-error', 'n_clicks')],
-    [dash.State('home-url-store-error', 'data')],
-    prevent_initial_call=True
-)
 
 @callback(
     [Output('availability-plot', 'figure'),
