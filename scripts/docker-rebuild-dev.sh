f9a9434979db4691d1cd18ad2c80816843e5273d
elpatron68 <m.busche@gmail.com>
2025-09-04 07:06:28 +0000

    cd

--- scripts/docker-rebuild-dev.sh
+++ scripts/docker-rebuild-dev.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker compose -f docker-compose-dev.yml down && docker compose -f docker-compose-dev.yml builddocker compose -f docker-compose-dev.yml up -d
+cd .. && docker compose -f docker-compose-dev.yml down && docker compose -f docker-compose-dev.yml builddocker compose -f docker-compose-dev.yml up -d
