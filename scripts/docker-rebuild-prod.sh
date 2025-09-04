3035b4049d12ea3f52872e6043a9787e2724099f
elpatron68 <m.busche@gmail.com>
2025-09-04 07:07:58 +0000

    &&

--- scripts/docker-rebuild-prod.sh
+++ scripts/docker-rebuild-prod.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker compose -f docker-compose.yml down && docker compose -f docker-compose.yml builddocker compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml down && docker compose -f docker-compose.yml build && docker compose -f docker-compose.yml up -d
